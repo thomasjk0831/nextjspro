c0d326d5fe138d5e95adfbffad6d10158450a93a
Thomas Kim <thomaskim08310@gmail.com>
2024-10-09 10:26:01 -0400

    folder

--- my-app/app/layout.jsx
+++ my-app/app/layout.jsx
@@ -1,5 +1,12 @@
 import '@/assets/styles/global.css'
 
+// efficient SEO from server side
+export const metadata = {
+    title: "ground bnb",
+    keywords: "rental, property, real estate",
+    description: "Find the perfect rental"
+}
+
 const MainLayout = ({ children }) => {
     return <html>
         <body>
