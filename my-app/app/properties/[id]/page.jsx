3f67bd691f38f8ad4a5daee66d1700176662dc3c
Thomas Kim <thomaskim08310@gmail.com>
2024-10-16 08:37:02 -0400

    server params

--- my-app/app/properties/[id]/page.jsx
+++ my-app/app/properties/[id]/page.jsx
@@ -1,5 +1,5 @@
-const PropertyPage = () => {
-    return <div>Property page</div>;
+const PropertyPage = ({ params }) => {
+    return <div>Property page {params.id}</div>;
 }
  
 export default PropertyPage;
